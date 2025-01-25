6c5e75b8af66ed05fa388f27c019b1df3c93d58a
zzywysm <zzy@zzywysm.com>
2025-01-25 17:59:59 -0500

    linux-asahi: asahi-6.12.4-1 -> asahi-6.12.10-2

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -86,7 +86,7 @@ let
     (linuxKernel.manualConfig rec {
       inherit stdenv lib;
 
-      version = "6.12.4-asahi";
+      version = "6.12.10-asahi";
       modDirVersion = version;
       extraMeta.branch = "6.12";
 
@@ -94,8 +94,8 @@ let
         # tracking: https://github.com/AsahiLinux/linux/tree/asahi-wip (w/ fedora verification)
         owner = "AsahiLinux";
         repo = "linux";
-        rev = "asahi-6.12.4-1";
-        hash = "sha256-0JJtJWM0eNKcBMkNRCOA6Vpoi6Ca1QCOlsmSQKRHAUA=";
+        rev = "asahi-6.12.10-2";
+        hash = "sha256-lfD+Vok8AM8GGb6u5p76lN9QDS+kYax41MiHUuA3dU8=";
       };
 
       kernelPatches = [
